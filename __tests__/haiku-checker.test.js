d4ed98e143c3af01f8f287f1fba12d3096e036fc
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-15 16:13:52 -0700

    Add haikuChecker() spec
    Co-authored-by: Zack Rutledge <Thorgrim88@gmail.com>

--- __tests__/haiku-checker.test.js
+++ __tests__/haiku-checker.test.js
@@ -1,8 +1,22 @@
-import syllableCounter from '../src/js/haiku-checker.js';
+import { syllableCounter, haikuChecker } from '../src/js/haiku-checker.js';
 
-describe('syllableCounter()', () => {
-  test('should correctly count the number of vowels in a word', () => {
-    const word = "chocolate"
-    expect(syllableCounter(word)).toEqual(4);
+// describe('syllableCounter()', () => {
+//   // test('should correctly count the number of vowels in a word', () => {
+//   //   const word = "chocolate"
+//   //   expect(syllableCounter(word)).toEqual(4);
+//   // });
+
+//   test('should correctly count the number of syllables in a word', () => {
+//     const word = "chocolate"
+//     expect(syllableCounter(word)).toEqual(4);
+//   });
+// });
+
+describe('haikuChecker()', () => {
+  test('should correctly count the number of syllables', () => {
+    const line1 = "Haikus ar easy."
+    const line2 = "But somtims they don't mak sens."
+    const line3 = "Refrigerator."
+    expect(haikuChecker(line1, line2, line3)).toEqual(true);
   });
 });
